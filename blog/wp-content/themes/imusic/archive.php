306414ae911de9868b1ba21f72b01cdc6bb978d8
kitystudio <yuheki.kitano@gmail.com>
2021-05-26 16:14:51 +0900

    category

--- blog/wp-content/themes/imusic/archive.php
+++ blog/wp-content/themes/imusic/archive.php
@@ -1,9 +1,33 @@
 <?php
 /* 
 archive.php
- */
+*/
 
- get_header();
+get_header();
 
+global $post;
+$categories = get_the_category( $post->ID );
+// var_dump( $categories );
 
- get_footer();
+?>
+<article id="archive">
+  <div class="wrapper post_wrapper">
+    <h1 class="post_title">category: <?=$categories[0]->name;?></h1>
+<?php
+if ( have_posts() ) {
+	while ( have_posts() ) {
+		the_post(); 
+    ?>
+    <div class="post">
+      <h2 class="title"><a href="<?=get_permalink();?>"><?php the_title(); ?></a></h2>
+      <?php the_excerpt(); ?>
+    </div>
+    <?php
+		
+	}
+}
+?>
+  </div>
+</article>
+<?php
+get_footer();
