c428fc4fa2b29bd6c80139c3a15f712d75b17064
kitystudio <yuheki.kitano@gmail.com>
2021-06-04 11:05:11 +0900

    Minor modify

--- blog/wp-content/themes/imusic/index.php
+++ blog/wp-content/themes/imusic/index.php
@@ -8,19 +8,19 @@ get_header();?>
         <div class="wrapper">
           <div class="flex_hori">
             <div class="member flex_vert">
-              <p class="img_block"><img src="https://lh3.googleusercontent.com/pw/ACtC-3f5ZRzn10c74BWeyV5U7mpsRkY7Y64oFhi3RBVwWHi5BCeuaRUkvmrQ1AY680lm39sVIAEPmioIihxkmXEyPoA3hFzY-y1Hfw7efHxwVqoGLafwnAGSIz8aZ3dTKxBBeV2kErD0SQi1Hnls_g-PRrM4Zw=s220-no?authuser=0" width="220" alt="imusic"></p>
+              <p class="img_block"><img src="https://lh3.googleusercontent.com/pw/ACtC-3f5ZRzn10c74BWeyV5U7mpsRkY7Y64oFhi3RBVwWHi5BCeuaRUkvmrQ1AY680lm39sVIAEPmioIihxkmXEyPoA3hFzY-y1Hfw7efHxwVqoGLafwnAGSIz8aZ3dTKxBBeV2kErD0SQi1Hnls_g-PRrM4Zw=s220-no?authuser=0" width="220" height="220" alt="imusic"></p>
               <h2 class="name">imusic</h2>
               <p class="descri">Guitar</p>
               <p class="equip">Fender ST314, Fender STM, VOX MV50Rock, JimDunlop CryBaby, Boss DD-500, Digitech Genesis3</p>
             </div>
             <div class="member flex_vert">
-              <p class="img_block"><img src="https://lh3.googleusercontent.com/pw/ACtC-3cwePkHeLKUagr31BEHiN1UKiqcJaU1Yhn6djIpCG0yWnLCGxn4RUbmrBrnq62H-LNGy0PPAyv1MrCxbe6k5zY5o086mXsxQtulllHKp0ylWmSk7Rv-f41ivMz4Io5_yJkI1e0kMTC3ibRijXtWFWlu9w=s220-no?authuser=0" width="220" alt="imusic"></p>
+              <p class="img_block"><img src="https://lh3.googleusercontent.com/pw/ACtC-3cwePkHeLKUagr31BEHiN1UKiqcJaU1Yhn6djIpCG0yWnLCGxn4RUbmrBrnq62H-LNGy0PPAyv1MrCxbe6k5zY5o086mXsxQtulllHKp0ylWmSk7Rv-f41ivMz4Io5_yJkI1e0kMTC3ibRijXtWFWlu9w=s220-no?authuser=0" width="220" height="220" alt="imusic"></p>
               <h2 class="name">imusic</h2>
               <p class="descri">Bass, Vocal</p>
               <p class="equip">Eden WTDI, Behringer XM8500</p>
             </div>
             <div class="member flex_vert">
-              <p class="img_block"><img src="https://lh3.googleusercontent.com/pw/ACtC-3f7mdIEK-PLcWKurOjQOmT8RU4XUsl8yJnqkaRQluUBPbvfJ-zZiWXPxmMCwL6Zlt6nFZT1ecSbcBP_I3YAdxeu4QusPKswaOvjfyywR-MWWphH9my-rcJSlJV7JGFhFiHQsPYJMsyF-BvrNL1cG7oMMQ=s220-no?authuser=0" width="220" alt="imusic"></p>
+              <p class="img_block"><img src="https://lh3.googleusercontent.com/pw/ACtC-3f7mdIEK-PLcWKurOjQOmT8RU4XUsl8yJnqkaRQluUBPbvfJ-zZiWXPxmMCwL6Zlt6nFZT1ecSbcBP_I3YAdxeu4QusPKswaOvjfyywR-MWWphH9my-rcJSlJV7JGFhFiHQsPYJMsyF-BvrNL1cG7oMMQ=s220-no?authuser=0" width="220" height="220" alt="imusic"></p>
               <h2 class="name">imusic</h2>
               <p class="descri">Midi Program</p>
               <p class="equip">Ablton Live9, Ableton Push2, Korg microKEY, Apple iMac, NativeInstruments KomplateAudio6</p>
